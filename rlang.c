13768db38118fd01da5fee7d4d32bdefb7ab7cd9
Hannes Astrom <dat15has@varg-5.student.lth.se>
2016-02-10 09:12:20 +0100

    Basic impl. but not working

--- rlang.c
+++ rlang.c
@@ -6,6 +6,19 @@ void insertAfter(char* array, int length, char insert, int position)
 	char* 
 }
 */
+int isConsonant(char c);
+int countConsonants(char* array, int length);
+
+int isConsonant(char c)
+{
+	if(c=='a'||c=='A'||c=='e'||c=='E'||c=='i'||c=='I'||c=='o'||c=='O'||c=='u'||c=='U' || c<65 || c>122 || (c>90 && c<97))
+        {
+                return 0;
+        } else {
+                return 1;
+	}
+}
+
 int countConsonants(char* array, int length)
 {
 	char c;
@@ -14,16 +27,18 @@ int countConsonants(char* array, int length)
 	for(i = 0; i < length; i++)
 	{
 		c = array[i];
-		if(c=='a'||c=='A'||c=='e'||c=='E'||c=='i'||c=='I'||c=='o'||c=='O'||c=='u'||c=='U' || c<65 || c>22 || (c>90 && c<97))
+		if(isConsonant(c))
 		{
-			continue;
-		} else {
 			count++;
+		} else {
+			continue;
 		}
 	}
 
 	return count;
 }
+
+
 
 int main(int argc, char** argv)
 {
@@ -32,8 +47,10 @@ int main(int argc, char** argv)
 	char* ch;
 	char* longArray;
 	char* words = malloc(1000 * sizeof(char));
+	printf(argv[1]);
+	printf("\n");
 	inputFile = fopen(argv[1], "r");
-	printf("File opened");
+	printf("File opened\n");
 	while( (ch = fgetc(inputFile)) != EOF)
 	{
 		words[index] = ch;
@@ -41,8 +58,21 @@ int main(int argc, char** argv)
 	}
 	words[index] = '\0';
 	//Length with EOF
-	ch = malloc((index + (countConsonants(words,index) * 2)) * sizeof(char));	
+	longArray = malloc((index + (countConsonants(words,index) * 2)) * sizeof(char));
+	index = 0;
+	while(longArray[index] != EOF)
+	{
+		if(isConsonant(longArray[index])){
+			char temp = longArray[index];
+			longArray[index + 1] = 'o';
+			longArray[index + 2] = temp;
+			index += 3;
+		} else {
+			index++;
+		}
+	}
+	printf(longArray);	
 	fclose(inputFile);
-	printf("File closed");
+	printf("File closed\n");
 	return 0;
 }
